da4bd0696d09738936428ee9a97925301c0c1d01
Yang.Y <yang@yangyubo.com>
2015-05-24 16:46:20 +0800

    Update setup.py
    Update repo url, and other info

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='blogdown',
     version='1.2.0',
-    author='Brant Young',
-    author_email='brant.young@gmail.com',
+    author='Blogdown Team',
+    author_email='yang@yangyubo.com',
     include_package_data = True,
     packages=['blogdown', 'blogdown.modules'],
     description='a simple static blog generator',
     long_description=open('README.rst').read(),
     license='BSD License',
-    url='https://github.com/brantyoung/blogdown/',
+    url='https://github.com/blogdown/blogdown/',
     entry_points = {
         'console_scripts': ['run-blogdown = blogdown.cli:main'],
     },
