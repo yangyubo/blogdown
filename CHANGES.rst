2865098207cc31067457896b200e9adce05e5a1e
Thomas Gläßle <t_glaessle@gmx.de>
2015-05-24 04:35:13 +0200

    Blogdown 1.2.0

---
+++ CHANGES.rst
@@ -0,0 +1,9 @@
+CHANGES
+-------
+
+1.2.0
+~~~~~
+Date: 24.05.2015
+
+- fix background colors in code blocks
+- add plugin support
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='blogdown',
-    version='1.1.4',
+    version='1.2.0',
     author='Brant Young',
     author_email='brant.young@gmail.com',
     include_package_data = True,
